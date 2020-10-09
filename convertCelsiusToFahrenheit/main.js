2e3073012cd53260d00ca59b49f41bdfb33d2d66
Furkan <furkvnclk@gmail.com>
2020-10-09 14:40:57 +0300

    create algorithm to convert from celsius to fahrenheit

---
+++ convertCelsiusToFahrenheit/main.js
@@ -0,0 +1,8 @@
+function convertToFahren(celsius) {
+    return (celsius * (9 / 5)) + 32;
+}
+
+convertToFahren(30)
+
+// Uncomment the line below to see the result on the console.
+// console.log(convertToFahren(30));
